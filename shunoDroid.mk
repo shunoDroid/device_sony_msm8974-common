9fba9fb0408faa3212985842ffb4fff5816896f3
Shun Terabayashi <shunonymous@gmail.com>
2019-07-03 00:25:25 +0900

    Include our vendor repo

--- shunoDroid.mk
+++ shunoDroid.mk
@@ -6,3 +6,5 @@ ifeq (,$(filter $(TARGET_KERNEL_SOURCE),))
   KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-unknown-linux-androideabi-$(KERNEL_TOOLCHAIN_VERSION)/bin
   KERNEL_TOOLCHAIN_PREFIX := arm-unknown-linux-androideabi-
 endif
+
+-include vendor/shunodroid/shunodroid.mk
