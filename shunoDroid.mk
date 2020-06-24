f9c5513ad67fdfef6426821b6ce6e31727f611cf
Shun Terabayashi <shunonymous@gmail.com>
2020-06-24 16:03:08 +0900

    Upgrade gcc

--- shunoDroid.mk
+++ shunoDroid.mk
@@ -1,6 +1,6 @@
 # Kernel properties
 ifeq (,$(filter $(TARGET_KERNEL_SOURCE),))
-  KERNEL_TOOLCHAIN_VERSION := 7.x
-  KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-unknown-linux-androideabi-$(KERNEL_TOOLCHAIN_VERSION)/bin
-  KERNEL_TOOLCHAIN_PREFIX := arm-unknown-linux-androideabi-
+  KERNEL_TOOLCHAIN_VERSION := 10.1.0
+  KERNEL_TOOLCHAIN_PREFIX := arm-unknown-linux-gnueabi-
+  KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/$(HOST_OS)-x86/arm/${KERNEL_TOOLCHAIN_PREFIX}$(KERNEL_TOOLCHAIN_VERSION)/bin
 endif
